65dde977938595c1721d6244966efcf434ebf9a1
ganyuke <personalemailforedits@gmail.com>
2022-10-23 19:37:10 -0700

    Add bodyParser to interpret POST body

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/api/sos', (req, res) => {
     console.log('Hit by post request from IP: \"' + req.ip +  '\" :(');
-    res.end("Request recieved.")
+    console.log("Request:",req.body)
+    res.end("Request recieved. Request was: \"" + req.body + "\"")
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
