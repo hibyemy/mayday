73449f126adf4825a7398e3a53791994ba12d223
ganyuke <personalemailforedits@gmail.com>
2022-10-23 19:11:36 -0700

    post api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.post('/api/sos', (request, response) => {
-    console.log('Hit by post request :(');
+app.post('/api/sos', (req, res) => {
+    console.log('Hit by post request from IP: \"' + req.ip +  '\" :(');
+    res.end("Request recieved.")
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
