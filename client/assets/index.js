c91a81cc165025290b11ba56950c06c70b6cdd2c
ganyuke <personalemailforedits@gmail.com>
2022-10-25 01:17:42 -0700

    Implement dynamically adding phone numbers, JSON

---
+++ client/assets/index.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("emergency-form");
+form.addEventListener("submit", submit);
+
+var errorText = document.getElementById("error-response");
+
+function errorDisplay(error) {
+    errorText.innerHTML = '<span>'+error+'</span>';
+}
+
+function submit(submit_form) {
+    submit_form.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:7116/api/sos', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    var formData = new FormData(submit_form.target);
+    var jsonFile = {'contacts':[]};
+    formData.forEach((v, i) => {
+        if (i=='phonenumber'){
+            jsonFile['contacts'].push(v);
+        }
+        else {
+            jsonFile[i] = v;
+        }
+    });
+    xhr.send(JSON.stringify(jsonFile));
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            errorDisplay(xhr.response)
+        }
+      }
+}
+
+const addNumberButton = document.getElementById("add-number");
+addNumberButton.addEventListener("click", addNewNumberForm);
+
+let num_forms = 2;
+
+function addNewNumberForm(click) {
+    if (num_forms > 5) {
+        errorDisplay('Error: Max 5 contacts.')
+        return;
+    }
+    var dummy = "<label for=\"phonenumber"+num_forms+"\">Emergency contact #"+num_forms+":</label><br><input type=\"text\" id=\"phonenumber"+num_forms+"\" name=\"phonenumber\"  placeholder=\"+11231231234\"><br><br>";
+    document.getElementById('phone-numbers').innerHTML += dummy;
+    num_forms += 1;    
+}
--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
-const fowardMessage = require("./sms")
+const fowardMessage = require("./sms");
 const express = require('express');
+const cors = require('cors');
 const app = express();
-app.use(express.urlencoded())
-app.set('trust proxy', 'loopback')
+app.use(express.json());
+app.set('trust proxy', 'loopback');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+var origin_whitelist = process.env.HOST.split(' ')
+
+var corsOptions = {
+    origin: origin_whitelist,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.post('/api/sos', (req, res) => {
     console.log('Hit by post request from IP: \"' + req.ip +  '\" :(');
-    console.log("Request:",req.body)
-    json = req.body
-    message = json.message
-    var recievers = []
-    recievers.push(json.phonenumber1)
-    recievers.push(json.phonenumber2)
-    recievers.push(json.phonenumber3)
-    recievers.push(json.phonenumber4)
-    recievers.push(json.phonenumber5)
-    var response = fowardMessage.send(message, recievers).toString()
-    res.end("Message recieved. Failed to send to: " + response)
+    console.log("Request:", req.body);
+    var json = req.body;
+    var message = json.message;
+    var contacts = json.contacts;
+    var response = fowardMessage.send(message, contacts).toString();
+    res.setHeader('Content-Type', 'text/plain')
+    res.end("Message recieved." + response);
 });
 
-app.listen(7116, () => console.log('Started.'));
+app.listen(7116, () => console.log('Listening on port 7116.'));
