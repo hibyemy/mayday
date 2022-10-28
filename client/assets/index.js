ced7fa80d01e435af146e7785016dd05732088d0
Hibyemy <bc2005530@gmail.com>
2022-10-28 00:25:33 -0700

    Main

--- client/assets/index.js
+++ client/assets/index.js
@@ -45,3 +45,19 @@ function addNewNumberForm() {
     document.getElementById('phone-numbers').innerHTML += dummy;
     num_forms += 1;    
 }
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+console.log(x.innerHTML);
